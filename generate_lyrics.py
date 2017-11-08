a66f8c4c5c18298176e690e588e61c038c67315d
giacomo.vannucchi@flubit.com <giacomo@ITs-MacBook-Pro.local>
2017-11-08 12:26:46 +0000

    improving print lyric

--- generate_lyrics.py
+++ generate_lyrics.py
@@ -4,5 +4,15 @@ from markov_python.cc_markov import MarkovChain
 if __name__ == '__main__':
 
     mc = MarkovChain()
+
     mc.add_file('lyrics.txt')
-    print mc.generate_text()
+
+    lyric = mc.generate_text()
+    lyric = ' '.join(lyric)
+
+    lyric = ''.join([i for i in lyric if not i.isdigit()])
+    print '---------------------------'
+    print '---------------------------'
+    print lyric
+    print '---------------------------'
+    print '---------------------------'
